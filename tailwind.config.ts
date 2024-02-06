fb6994730c5e9524d715c68710dae167c159af93
LICF01 <lucascubilla@gmail.com>
2024-02-06 19:45:18 -0300

    Set serif and sans-serif fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  darkMode: "class",
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        serif: ['var(--font-crimsonPro)', 'serif'],
+        sans: ['var(--font-raleway)', 'sans-serif'],
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        background: "hsl(var(--background) / <alpha-value>)",
-        foreground: "hsl(var(--foreground) / <alpha-value>)",
-        border: "hsl(var(--border) / <alpha-value>)",
-        accent: "hsl(var(--accent) / <alpha-value>)",
-        "accent-dark": "hsl(var(--accent-dark) / <alpha-value>)",
-        "accent-light": "hsl(var(--accent-light) / <alpha-value>)",
+        background: 'hsl(var(--background) / <alpha-value>)',
+        foreground: 'hsl(var(--foreground) / <alpha-value>)',
+        border: 'hsl(var(--border) / <alpha-value>)',
+        accent: 'hsl(var(--accent) / <alpha-value>)',
+        'accent-dark': 'hsl(var(--accent-dark) / <alpha-value>)',
+        'accent-light': 'hsl(var(--accent-light) / <alpha-value>)',
       },
     },
   },
