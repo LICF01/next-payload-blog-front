ec9a02efdbe5431675962ff2dc9f9da5b4c0fc4c
LICF01 <lucascubilla@gmail.com>
2024-02-05 21:03:02 -0300

    Add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
+        border: "hsl(var(--border) / <alpha-value>)",
+        accent: "hsl(var(--accent) / <alpha-value>)",
+        "accent-content": "hsl(var(--accent-content) / <alpha-value>)",
+        "accent-dark": "hsl(var(--accent-dark) / <alpha-value>)",
+        "accent-light": "hsl(var(--accent-light) / <alpha-value>)",
+      },
     },
   },
   plugins: [],
