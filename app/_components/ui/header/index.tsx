bdf25e5eb02daf505beb778cf6c42c0bcffd1452
LICF01 <lucascubilla@gmail.com>
2024-02-05 22:30:13 -0300

    Add prettier and prettier tailwind plugin Format a few files

--- app/_components/ui/header/index.tsx
+++ app/_components/ui/header/index.tsx
@@ -1,46 +1,46 @@
-import { fetchDocs } from "@/_api/fetchDocs";
-import { fetchHeader } from "@/_api/fetchGlobals";
-import { Page } from "@/_types/payload-types";
-import React from "react";
+import { fetchDocs } from '@/_api/fetchDocs'
+import { fetchHeader } from '@/_api/fetchGlobals'
+import { Page } from '@/_types/payload-types'
+import React from 'react'
 
 const getHeader = async () => {
   try {
-    const Header = await fetchHeader();
-    return Header;
+    const Header = await fetchHeader()
+    return Header
   } catch (error) {
-    return [];
+    return []
   }
-};
+}
 
 const getNavItems = async () => {
   try {
-    const pages = await fetchDocs<Page>("pages");
-    return pages?.map(({ slug, name }) => ({ slug, name }));
+    const pages = await fetchDocs<Page>('pages')
+    return pages?.map(({ slug, name }) => ({ slug, name }))
   } catch (error) {
-    return [];
+    return []
   }
-};
+}
 
 export default async function Header() {
-  const [navItems, header] = await Promise.all([getNavItems(), getHeader()]);
+  const [navItems, header] = await Promise.all([getNavItems(), getHeader()])
 
   return (
     <header>
-      <div className="flex items-center justify-between max-w-7xl mx-auto">
+      <div className='mx-auto flex max-w-7xl items-center justify-between'>
         <a
-          href="/"
-          className="font-semibold uppercase hover:text-primary group inline-flex py-6 text-2xl tracking-widest transition focus-visible:outline-none"
+          href='/'
+          className='hover:text-primary group inline-flex py-6 text-2xl font-semibold uppercase tracking-widest transition focus-visible:outline-none'
         >
           <span>{header.siteName}</span>
         </a>
-        <div className="flex items-center">
+        <div className='flex items-center'>
           <nav>
-            <ul className="flex gap-4">
+            <ul className='flex gap-4'>
               {navItems
                 ? navItems.map(({ slug, name }) => (
                     <li>
                       <a href={`/${slug}`}>
-                        <span className="group-hover:underline">{name}</span>
+                        <span className='group-hover:underline'>{name}</span>
                       </a>
                     </li>
                   ))
@@ -50,5 +50,5 @@ export default async function Header() {
         </div>
       </div>
     </header>
-  );
+  )
 }
---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
