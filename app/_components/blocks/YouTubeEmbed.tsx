f732d111aa6c8f655162d2d5290f062c6a7d2b6e
LICF01 <lucascubilla@gmail.com>
2024-02-11 00:55:23 -0300

    Add play button

--- app/_components/blocks/YouTubeEmbed.tsx
+++ app/_components/blocks/YouTubeEmbed.tsx
@@ -1,18 +1,21 @@
-'use client'
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
+'use client';
+import Image from 'next/image';
+import { useState } from 'react';
+import { Button } from 'react-aria-components';
+
+import { PlayCircle } from 'lucide-react';
 
 type YouTubeEmbedProps = {
-  id: string | null
-  title: string
-  description?: string | null
-  blockName?: string | null
-  blockType: 'hero'
-}
+  id: string | null;
+  title: string;
+  description?: string | null;
+  blockName?: string | null;
+  blockType: 'hero';
+};
 
 export default function YouTubeEmbed(props: YouTubeEmbedProps) {
-  const { id, title } = props
-  const [showVideo, setShowVideo] = useState(false)
+  const { id, title } = props;
+  const [showVideo, setShowVideo] = useState(false);
   return (
     <div>
       {showVideo ? (
@@ -27,18 +30,21 @@ export default function YouTubeEmbed(props: YouTubeEmbedProps) {
           className='aspect-[16/9] h-full w-full p-0'
         />
       ) : (
-        <div className='group relative  aspect-[16/9] w-full cursor-pointer bg-red-400'>
+        <div className='group relative flex aspect-[16/9] w-full  cursor-pointer items-center justify-center'>
           <Image
             src={`https://img.youtube.com/vi/${id}/hqdefault.jpg`}
             alt=''
             width={560}
             height={315}
-            className='h-full w-full opacity-80'
+            className='h-full w-full opacity-80 filter'
             style={{ objectFit: 'cover', margin: 0 }}
             onClick={() => setShowVideo(true)}
           />
+          <Button className='absolute rounded-full bg-accent text-background'>
+            <PlayCircle size={72} strokeWidth={1} />
+          </Button>
         </div>
       )}
     </div>
-  )
+  );
 }
